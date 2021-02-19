f8981a79f9572f8367039dbec6103d0594ac4ad5
ShivsAndGiggles <royjairusraj@hotmail.com>
2021-02-19 15:49:57 +0000

    v4 Creating tests

---
+++ app/src/androidTest/java/com/example/fyp1/DailyTasksActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.fyp1;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DailyTasksActivityTest {
+
+    @Test
+    void onClick() {
+    }
+
+    @Test
+    void onItemClick() {
+    }
+}
--- app/src/main/java/com/example/fyp1/NewDailyActivity.java
+++ app/src/main/java/com/example/fyp1/NewDailyActivity.java
@@ -4,14 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class NewDailyActivity extends AppCompatActivity {
 
