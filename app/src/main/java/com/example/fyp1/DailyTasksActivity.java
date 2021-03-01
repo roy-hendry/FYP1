78e772a5f83db3505489539c2e477dcba4a0949e
ShivsAndGiggles <royjairusraj@hotmail.com>
2021-03-01 16:20:17 +0000

    v6 Dailies tickboxes made
    Dailies have had their tickboxes made and they can be deleted by a onLongClickListener

--- app/src/main/java/com/example/fyp1/DailyTasksActivity.java
+++ app/src/main/java/com/example/fyp1/DailyTasksActivity.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
-public class DailyTasksActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class DailyTasksActivity extends AppCompatActivity implements OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private Button newDailyButton;
     private EditText taskEditText;
@@ -34,11 +35,13 @@ public class DailyTasksActivity extends AppCompatActivity implements View.OnClic
         taskList = findViewById(R.id.taskList);
 
         tasks = FileHandler.readDailiesData(this);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, tasks); //check if I can make this into a checkbox list instead of just a normal list
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, tasks); //check if I can make this into a checkbox list instead of just a normal list
         taskList.setAdapter(adapter);
 
         newDailyButton.setOnClickListener(this);
         taskList.setOnItemClickListener(this);
+        taskList.setOnItemLongClickListener(this);
+
 
         toDoListPageButton = (Button) findViewById(R.id.toDoListPageButton);
         toDoListPageButton.setOnClickListener(new View.OnClickListener() {
@@ -67,10 +70,20 @@ public class DailyTasksActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        CheckedTextView v = (CheckedTextView) view;
+        v.setChecked(!v.isChecked());
+        adapter.notifyDataSetChanged();
+
+        Toast.makeText(this, "Task Completed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         tasks.remove(position);
 
         adapter.notifyDataSetChanged();
         FileHandler.writeDailiesData(tasks, this);
-        Toast.makeText(this, "Task Completed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Task Removed", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
